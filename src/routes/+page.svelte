5d48dd136d6ebb52861569744d193c2b20703224
Eric Brown <eric.brown8787@gmail.com>
2024-03-20 22:29:12 -0400

    Configure Svelte and port eleventy version of site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,102 @@
+<article class="hero">
+  <h2 class="hero-heading">
+    <span class="hero-heading__greeting">Hi,</span>
+    <span class="hero-heading__intro">I'm Eric Brown</span>
+  </h2>
+  <p class="hero-text">
+    I’m a recent graduate of Raritan Valley Community College and an aspiring
+    software developer, UI designer and game developer. I love bringing creative
+    projects to life with code.
+  </p>
+  <div class="hero-image">
+    <p class="placeholder-text">Photo</p>
+  </div>
+  <button class="hero-cta link-button">
+    <a href="/about" class="link-button__link"
+      ><span class="link-button__text">More about me</span></a
+    >
+  </button>
+</article>
+
+<style>
+
+  :root{
+    --first-button-color: #00FF00,
+    --second-button-color:  #FF0000,
+    --third-button-color: #0000FF
+  }
+
+  .hero{
+    display: grid;
+    gap: 1rem;
+  }
+  .hero-heading{
+    grid-column: span 2;
+  }
+
+  .hero-heading > *{
+    display: block;
+  }
+  .hero-text{
+    grid-column: 1 / span 2;
+  }
+  .hero-cta{
+    grid-column: span 2;
+    justify-self: center;
+  }
+  .hero-cta {
+    border: none;
+    border-radius: none;
+    height: fit-content;
+    width: fit-content;
+    background: none;
+  }
+  .hero-image{
+    grid-column: span 2;
+    grid-row: 2;
+    background-color: black;
+    height: 300px;
+    width: 250px;
+    justify-self: center;
+  }
+  .placeholder-text{
+    color: white;
+  }
+  .link-button__text {
+    display: block;
+  }
+
+  .link-button__link {
+    border: 4px outset black;
+    background: green;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100%;
+    height: 90px;
+    width: 90px;
+    padding: 1rem;
+    text-decoration: none;
+  }
+  a:hover {
+    font-weight: unset;
+  }
+  .link-button:hover .link-button__link {
+    transition: transform 100ms ease-in;
+    transform: scale(1.1, 1.1);
+  }
+
+  .link-button__link:active {
+    border: 4px inset black;
+  }
+  @media (min-width: 768px){
+    .hero-image{
+      grid-column: 2/span 1;
+      grid-row: unset;
+    }
+    .hero-text{
+      grid-row: 2;
+      grid-column: 1 / span 1;
+    }
+  }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,187 @@
+<script>
+    import logo from '$lib/images/logo.svg'
+</script>    
+    <header class="page-header">
+      <article class="logo">
+        <img src="{logo}" alt="My logo" class="logo__image" />
+        <h1 class="logo__heading">Eric Brown</h1>
+      </article>
+      <label for="hamburger-menu" class="hamburger-menu-input">
+        <img class="nav-open-input" src="{logo}" alt="" />
+        <p class="nav-close-input">X</p>
+      </label>
+      <input
+        type="checkbox"
+        name="hamburger-menu"
+        id="hamburger-menu"
+        unchecked
+      />
+
+      <nav class="site-nav">
+        <ul class="site-nav-links">
+          <li class="site-nav-links__link">
+            <a href="/">Home</a>
+          </li>
+          <li class="site-nav-links__link">
+            <a href="/about">About</a>
+          </li>
+          <li class="site-nav-links__link">
+            <a href="/">Portfolio</a>
+          </li>
+          <li class="site-nav-links__link">
+            <a href="/">Resume</a>
+          </li>
+          <li class="site-nav-links__link">
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <style>
+        /* Resets */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+  color: black;
+}
+
+a:hover {
+  font-weight: 400;
+}
+
+nav ul {
+  list-style: none;
+}
+
+nav ul li a {
+  text-decoration: none;
+}
+
+/* Full page */
+body {
+  background-color: #f0fdfa;
+}
+
+main {
+  padding: 1rem;
+}
+
+/* Page Header */
+.logo {
+  display: flex;
+  align-items: center;
+}
+.page-header {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  /* background-color: rgb(0, 0, 0, 0.1); */
+  padding-inline: 10px;
+  padding-block: 10px;
+  border-block-end: 1px solid #f472b6;
+}
+
+.site-nav {
+  position: absolute;
+  top: -100px;
+  grid-row: 2;
+  grid-column: span 2;
+  text-align: center;
+  padding: 20px;
+}
+
+.site-nav-links {
+  display: none;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.site-nav-links__link {
+  font-size: 1.3rem;
+}
+
+.logo__image {
+  max-width: 50px;
+}
+
+/* Hamburger Menu for mobile nav*/
+#hamburger-menu {
+  display: none;
+}
+
+.hamburger-menu-input {
+  justify-self: right;
+}
+
+.nav-open-input {
+  width: 40px;
+}
+
+.nav-close-input {
+  display: none;
+  width: 40px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+}
+.page-header:has(#hamburger-menu:checked) .site-nav {
+  position: unset;
+}
+
+.page-header:has(#hamburger-menu:checked) .site-nav .site-nav-links {
+  display: flex;
+}
+
+.page-header:has(#hamburger-menu:checked)
+  .hamburger-menu-input
+  .nav-close-input {
+  display: block;
+}
+
+.page-header:has(#hamburger-menu:checked)
+  .hamburger-menu-input
+  .nav-open-input {
+  display: None;
+}
+
+/* Page Footer */
+.page-footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.footer-nav {
+  display: flex;
+  align-items: center;
+}
+
+@media (min-width: 768px) {
+  .hamburger-menu-input {
+    display: none;
+  }
+  .site-nav {
+    position: unset;
+    grid-row: 1;
+    grid-column: 2;
+  }
+  .site-nav-links {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+}
+    </style>
