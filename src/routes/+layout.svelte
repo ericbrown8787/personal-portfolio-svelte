9250c7b73b80b218377834629a25d03255a38a76
Eric Brown <eric.brown8787@gmail.com>
2024-04-02 23:15:00 -0400

    Add resume markup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,74 +1,69 @@
 <script>
-import Header from './Header.svelte'
-import Footer from './Footer.svelte'
+  import Header from "./Header.svelte";
+  import Footer from "./Footer.svelte";
 </script>
+
 <div class="app">
-    <Header />
+  <Header />
 
-    <main>
-        <slot />
-    </main>
+  <main>
+    <slot />
+  </main>
 
-    <Footer />
+  <Footer />
 </div>
 
 <style>
-/* CSS Global Variables */
-:global(:root) {
-    --panel-background-color: rgb(0,0,0,.8);
-    --main-text-color: #042F2E;
-    --main-background-color: #F0FDFA;
+  /* CSS Global Variables */
+  :global(:root) {
+    --panel-background-color: rgb(0, 0, 0, 0.8);
+    --main-text-color: #042f2e;
+    --main-background-color: #f0fdfa;
     --accent-color: #f472b6;
-    --first-button-color: #00FF00;
-    --second-button-color: #FF0000;
-    --third-button-color: #0000FF;
+    --first-button-color: #00ff00;
+    --second-button-color: #ff0000;
+    --third-button-color: #0000ff;
     --placeholder-text: whitesmoke;
     --accent-color: #f472b6;
-}
+  }
 
-@font-face {
-    font-family: 'Fira Code';
-    src: url('/fonts/FiraCode-VariableFont_wght.ttf');
-}
+  @font-face {
+    font-family: "Fira Code";
+    src: url("/fonts/FiraCode-VariableFont_wght.ttf");
+  }
 
-/* CSS Global Reset */
-:global(*,
-    *::before,
-    *::after) {
+  /* CSS Global Reset */
+  :global(*, *::before, *::after) {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
     font-family: "Fira Code", sans-serif;
-}
+  }
 
-:global(a) {
+  :global(:not(h1, h2, h3, h4, h5, dt)) {
+    line-height: 1.5;
+  }
+
+  :global(a) {
     text-decoration: none;
     color: var(--main-text-color);
-}
+  }
 
-:global(a:visited) {
+  :global(a:visited) {
     text-decoration: none;
     color: var(--main-text-color);
-}
+  }
 
-:global(nav ul) {
-    list-style: none;
-}
-
-:global(nav ul li a) {
-    text-decoration: none;
-}
-
-main{
+  main {
     margin-block-start: 8em;
-}
-/* Page Background */
-.app {
+  }
+  /* Page Background */
+  .app {
     background-color: var(--main-background-color);
     overflow: hidden;
-}
-/* Page Layout */
-.app {
+  }
+  /* Page Layout */
+  .app {
     min-height: 100vh;
-}
+  }
 </style>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,239 @@
+<article class="resume">
+  <h2 class="main-heading">Resume</h2>
+  <section class="resume-section objective">
+    <h3 class="section-heading">Objective</h3>
+    <p class="section-text">
+      Soon-to-be community college graduate with Interface Design and Web
+      Development A.A.S. and technical experience seeking a web development,
+      software development or IT internship or junior position.
+    </p>
+  </section>
+  <section class="resume-section education">
+    <h3 class="section-heading">Education</h3>
+    <ul class="resume-list">
+      <li class="resume-list-item">
+        <h4 class="item-heading">
+          A.A.S in Interface Design and Web Development
+        </h4>
+        <ul class="info-list">
+          <li class="info-item">Raritan Valley Community College</li>
+          <li class="info-item">Branchburg, New Jersey</li>
+          <li class="info-item">Expected Graduation May 2024</li>
+          <li class="info-item">4.0 Cumulative GPA</li>
+        </ul>
+
+        <dl class="details-list">
+          <dt class="details-heading">Relevant Coursework</dt>
+          <dd class="details-item">Computer Concepts and Programming</dd>
+          <dd class="details-item">Interactive Multimedia</dd>
+          <dd class="details-item">Web Development I & II</dd>
+          <dd class="details-item">Object-Oriented Programming</dd>
+          <dd class="details-item">Human-Computer-Interaction</dd>
+          <dd class="details-item">JavaScript Programming</dd>
+          <dd class="details-item">User Interface Programming</dd>
+          <dd class="details-item">Fundamentals of Game Engines</dd>
+          <dd class="details-item">Programming for Game Developers</dd>
+          <dd class="details-item">Python</dd>
+          <dd class="details-item">Advanced Game Design</dd>
+          <dd class="details-item">Interface Design Capstone</dd>
+        </dl>
+      </li>
+      <li class="resume-list-item">
+        <h4 class="item-heading">B.A. in English</h4>
+        <ul class="info-list">
+          <li class="info-item">Rutgers University</li>
+          <li class="info-item">New Brunswick, New Jersey</li>
+          <li class="info-item">Graduated May 2014</li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+  <section class="resume-section skills">
+    <h3 class="section-heading">Skills</h3>
+    <dl class="details-list">
+      <dt class="details-heading">Programming Languages</dt>
+      <dd class="details-item">JavaScript</dd>
+      <dd class="details-item">C#</dd>
+      <dd class="details-item">Python</dd>
+      <dd class="details-item">Java</dd>
+      <dd class="details-item">GDScript</dd>
+    </dl>
+    <dl class="details-list">
+      <dt class="details-heading">Markup, Data Formats and Stylesheets</dt>
+      <dd class="details-item">HTML</dd>
+      <dd class="details-item">Nunjucks</dd>
+      <dd class="details-item">Jinja</dd>
+      <dd class="details-item">CSS</dd>
+      <dd class="details-item">SASS</dd>
+      <dd class="details-item">JSON</dd>
+      <dd class="details-item">XML</dd>
+      <dd class="details-item">Markdown</dd>
+    </dl>
+    <dl class="details-list">
+      <dt class="details-heading">Runtimes, Libraries and Frameworks</dt>
+      <dd class="details-item">Node.js</dd>
+      <dd class="details-item">Express.js</dd>
+      <dd class="details-item">Eleventy</dd>
+      <dd class="details-item">Sveltekit</dd>
+      <dd class="details-item">Nunjucks</dd>
+      <dd class="details-item">Jinja</dd>
+      <dd class="details-item">Flask</dd>
+    </dl>
+    <dl class="details-list">
+      <dt class="details-heading">Software</dt>
+      <dd class="details-item">Git/GitHub</dd>
+      <dd class="details-item">Microsoft Visual Studio</dd>
+      <dd class="details-item">Visual Studio Code</dd>
+      <dd class="details-item">Adobe Creative Suite</dd>
+      <dd class="details-item">Unity</dd>
+      <dd class="details-item">Figma</dd>
+    </dl>
+    <dl class="details-list">
+      <dt class="details-heading">User Interface/UX</dt>
+      <dd class="details-item">Web Design</dd>
+      <dd class="details-item">Web Accessibility</dd>
+      <dd class="details-item">Game Design</dd>
+    </dl>
+  </section>
+  <section class="resume-section work-experience">
+    <h3 class="section-heading">Work Experience</h3>
+    <ul class="resume-list">
+      <li class="resume-list-item">
+        <h4 class="item-heading">IT/Accounts Receivable Specialist</h4>
+        <ul class="info-list">
+          <li class="info-item">Uriel Trucking LLC</li>
+          <li class="info-item">South Plainfield, New Jersey</li>
+          <li class="info-item">April 2022 to Present</li>
+        </ul>
+
+        <dl class="details-list">
+          <dt class="details-heading">Responsibilities</dt>
+          <dd class="details-item">
+            Configuring and maintaining computer and networking equipment
+          </dd>
+          <dd class="details-item">
+            Troubleshooting security systems and vehicle monitoring systems
+          </dd>
+          <dd class="details-item">Compiling invoices</dd>
+          <dd class="details-item">Building and maintaining spreadsheets</dd>
+          <dd class="details-item">Communicating with clients</dd>
+        </dl>
+      </li>
+      <li class="resume-list-item">
+        <h4 class="item-heading">Technician</h4>
+        <ul class="info-list">
+          <li class="info-item">Maximum Security Systems</li>
+          <li class="info-item">Marlboro, New Jersey</li>
+          <li class="info-item">March 2018 to April 2022</li>
+        </ul>
+
+        <dl class="details-list">
+          <dt class="details-heading">Responsibilities</dt>
+          <dd class="details-item">
+            Installing, testing, maintaining and repairing low-voltage systems
+          </dd>
+          <dd class="details-item">Instructing end users in use of software</dd>
+          <dd class="details-item">
+            Assembly, configuration and troubleshooting of PCs, mobile devices,
+            software and networking equipment
+          </dd>
+        </dl>
+      </li>
+    </ul>
+  </section>
+  <section class="resume-section projects">
+    <h3 class="section-heading">Projects</h3>
+    <ul class="resume-list">
+      <li class="resume-list-item">
+        <h4 class="item-heading">Style Stage</h4>
+        <p class="item-description">
+          Open Source Project - Contributed a CSS stylesheet.
+        </p>
+      </li>
+      <li class="resume-list-item">
+        <h4 class="item-heading">Detective Cats and the Gilded Secret</h4>
+        <p class="item-description">
+          Game(In development) - Developed a JSON-based character dialogue
+          markup/parsing system.
+        </p>
+      </li>
+    </ul>
+  </section>
+</article>
+
+<style>
+  li {
+    list-style: none;
+  }
+
+  .main-heading {
+    width: 100%;
+    content: "";
+    border-bottom: 1px solid var(--accent-color);
+  }
+
+  /* Margins and padding */
+  .resume-section {
+    margin-block: 3em;
+  }
+  .resume-list-item {
+    margin-block-end: 1em;
+  }
+  .main-heading,
+  .resume-section,
+  .resume-list,
+  .details-list {
+    margin-inline: 1em;
+  }
+  .item-heading {
+    font-size: 1rem;
+  }
+  .details-list,
+  .info-list {
+    margin-block-end: 0.3em;
+  }
+  .info-list {
+    padding-inline-start: 1rem;
+  }
+
+  /* Lists */
+  .info-item {
+    list-style: disc;
+  }
+
+  .details-heading {
+    font-weight: bold;
+    font-size: 0.9rem;
+  }
+  .details-heading::after {
+    content: ": ";
+  }
+  .details-item {
+    font-size: 0.8rem;
+  }
+  .details-item::before {
+    content: "+ ";
+    font-weight: bold;
+  }
+
+  @media (min-width: 768px) {
+    .info-item {
+      display: inline;
+    }
+    .info-item::before {
+      content: "|";
+    }
+    .info-item:first-child:before {
+      display: none;
+    }
+
+    .details-heading,
+    .details-item {
+      display: inline;
+    }
+
+    .details-item:first-of-type::before {
+      display: none;
+    }
+  }
+</style>
